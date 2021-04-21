726b04e80aabab7cd6bf5013d5e2b5b2f3c758a3
nowxel <nowxel@gmail.com>
2021-04-21 12:19:11 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:check_box/ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reactive Flutter',
+      theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          canvasColor: Colors.transparent
+      ),
+      //Our only screen/page we have
+      home: HomePage(title: 'My Todo List'),
+    );
+  }
+}
